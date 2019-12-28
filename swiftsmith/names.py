cac2b59545e3d804563496d2bc90bd727bf0e03e
jacobdweightman <jacobdweightman@gmail.com>
2019-12-28 01:27:08 -0600

    Add conditionals to grammar

--- swiftsmith/names.py
+++ swiftsmith/names.py
@@ -4,11 +4,25 @@ def _identifiers():
     """
     Enumerates identifiers for use in generated code.
     """
+
+    # List of reserved keywords, from
+    # https://docs.swift.org/swift-book/ReferenceManual/LexicalStructure.html
+    reserved = {
+        "associatedtype", "class", "deinit", "enum", "extension", "fileprivate", "func",
+        "import", "init", "inout", "internal", "let", "open", "operator", "private",
+        "protocol", "public", "rethrows", "static", "struct", "subscript", "typealias",
+        "var", "break", "case", "continue", "default", "defer", "do", "else",
+        "fallthrough", "for", "guard", "if", "in", "repeat", "return", "switch",
+        "where", "while", "as", "Any", "catch", "false", "is", "nil", "super", "self",
+        "Self", "throw", "throws", "true", "try", "_"
+    }
     letters = "abcdefghijklmnopqrstuvwxyz"
     length = 1
     while True:
         for i in product(letters, repeat=length):
-            yield "".join(i)
+            i = "".join(i)
+            if i not in reserved:
+                yield "".join(i)
         length += 1
 
 identifier = _identifiers()
--- swiftsmith/swift.py
+++ swiftsmith/swift.py
@@ -29,10 +29,13 @@ class SemanticPCFG(PCFG):
 
 S = Nonterminal("S")
 statement = Nonterminal("STATEMENT")
-expression = Nonterminal("EXPRESSION")
+def expression(t):
+    return Nonterminal(f"EXPRESSION<{t}>")
 declaration = Nonterminal("DECLARATION")
 funcdeclaration = Nonterminal("FUNC_DECLARATION")
-conditional = Nonterminal("CONDITIONAL")
+ifstatement = Nonterminal("IF_STATEMENT")
+conditionlist = Nonterminal("CONDITION_LIST")
+condition = Nonterminal("CONDITION")
 block = Nonterminal("BLOCK")
 returnstatement = Nonterminal("RETURN")
 
@@ -44,22 +47,32 @@ swift = SemanticPCFG(
         PProduction(S, (funcdeclaration, S), 0.3),
         PProduction(S, (funcdeclaration,), 0.4), # Guarantee at least one function
 
-        PProduction(statement, (expression,), 0.1),
-        PProduction(statement, (declaration,), 0.5),
-        #PProduction(statement, (conditional,), 0.5),
+        PProduction(statement, (declaration,), 0.8),
+        PProduction(statement, (ifstatement,), 0.2),
         PProduction(statement, (returnstatement,), 0.),
 
-        PProduction(expression, (expression, " + ", expression), 0.2),
-        PProduction(expression, (expression, " * ", expression), 0.2),
-        PProduction(expression, (Int(),), 0.3),
-        PProduction(expression, (Variable(),), 0.3),
+        PProduction(expression("Int"), (Int(), " + ", expression("Int")), 0.1),
+        PProduction(expression("Int"), (Variable(), " + ", expression("Int")), 0.1),
+        PProduction(expression("Int"), (Int(), " * ", expression("Int")), 0.1),
+        PProduction(expression("Int"), (Variable(), " * ", expression("Int")), 0.1),
+        PProduction(expression("Int"), (Int(),), 0.3),
+        PProduction(expression("Int"), (Variable(),), 0.3),
 
-        PProduction(declaration, (EOL(), "let ", Variable(declaration=True), " = ", expression), 0.3),
-        PProduction(declaration, (EOL(), "var ", Variable(declaration=True, mutable=True), " = ", expression), 0.7),
+        PProduction(expression("Bool"), (expression("Int"), " > ", expression("Int")), 0.2),
+        PProduction(expression("Bool"), (expression("Int"), " == ", expression("Int")), 0.2),
+        PProduction(expression("Bool"), ("true",), 0.2),
+        PProduction(expression("Bool"), ("false",), 0.2),
 
-        #PProduction(conditional, ("if ", Variable(), " > ", expression, " {\n", statement, "}\n"), 1.0),
+        PProduction(declaration, (EOL(), "let ", Variable(declaration=True), " = ", expression("Int")), 0.3),
+        PProduction(declaration, (EOL(), "var ", Variable(declaration=True, mutable=True), " = ", expression("Int")), 0.7),
 
-        PProduction(returnstatement, (EOL(), "return ", expression), 1.0),
+        PProduction(ifstatement, (EOL(), "if ", conditionlist, " {", block, EOL(), "}"), 0.5),
+        PProduction(ifstatement, (EOL(), "if ", conditionlist, " {", block, EOL(), "} else {", block, EOL(), "}"), 0.5),
+        PProduction(conditionlist, (condition,), 1.0),
+        PProduction(conditionlist, (condition, ", ", conditionlist), 0.2),
+        PProduction(condition, (expression("Bool"),), 1.0),
+
+        PProduction(returnstatement, (EOL(), "return ", expression("Int")), 1.0),
 
         PProduction(funcdeclaration, (EOL(), EOL(), "func ", Function(), " {", block, EOL(), "}"), 1.0),
 
