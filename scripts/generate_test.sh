4353ef0670fa9e09800f645658ced5f3b0e76f5f
jacobdweightman <jacobdweightman@gmail.com>
2020-04-04 18:18:44 -0500

    Move test generation into SwiftSmith

--- scripts/generate_test.sh
+++ scripts/generate_test.sh
@@ -2,13 +2,16 @@
 
 SEED=$1
 
-python3 -m swiftsmith ${SEED} -mr unnecessary-multiplication -o generated/module
+python3 -m swiftsmith \
+    ${SEED} \
+    -mr unnecessary-multiplication \
+    -o generated/Module \
+    --tests generated/test.swift
 
 sh scripts/generate_module.sh ModuleA -Onone
 sh scripts/generate_module.sh ModuleB -Osize
 
-python3 scripts/generate_harness.py > generated/harness.swift
-swiftc generated/harness.swift -I generated -L generated -lModuleA -lModuleB -o generated/test
+swiftc generated/test.swift -I generated -L generated -lModuleA -lModuleB -o generated/test
 
 install_name_tool -change libModuleA.dylib $(pwd)/generated/libModuleA.dylib generated/test
 install_name_tool -change libModuleB.dylib $(pwd)/generated/libModuleB.dylib generated/test
